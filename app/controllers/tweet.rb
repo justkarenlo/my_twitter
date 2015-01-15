458caea9d1ff6f8d1fe6763937eed711a4fb126d
justkarenlo <just.karenlo@gmail.com>
2015-01-15 15:22:36 -0800

    added user auth to tweet controller

--- app/controllers/tweet.rb
+++ app/controllers/tweet.rb
@@ -8,17 +8,29 @@ get "/" do
 end
 
 get "/tweets/new" do
-	@tweets = Tweet.all
-erb :"/tweets/new"
+  if user_logged_in?
+	  @tweets = Tweet.all
+    erb :"/tweets/new"
+  else
+    redirect '/login'
+  end
 end
 
 get "/tweets/:id" do
-	@tweet = Tweet.find(params[:id])
-erb :"/tweets/show"
+  if user_logged_in?
+	  @tweet = Tweet.find(params[:id])
+    erb :"/tweets/show"
+  else
+    redirect '/login'
+  end
 end
 
 
 post "/tweets" do
-	@tweets = Tweet.create(params[:tweet])
-redirect "/"
+  if user_logged_in?
+	  @tweets = Tweet.create(params[:tweet])
+    redirect '/'
+  else
+    redirect '/login'
+  end
 end
