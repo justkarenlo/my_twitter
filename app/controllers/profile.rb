631326b02a16dcbdb1505ffac5860d625ccaafa1
Charles <giardina.charles@gmail.com>
2015-01-16 08:00:05 -0800

    can view someone else's profile

--- app/controllers/profile.rb
+++ app/controllers/profile.rb
@@ -1,10 +1,27 @@
 get '/profile' do
   if user_logged_in?
-  @user = User.find(session['user_id'])
-  erb :'profile/index'
+    p 'profile owner'
+    p @profile_owner = User.find(session['user_id'])
+    p 'session id'
+    p session['user_id']
+    p 'that persons tweets'
+    p @profile_owner.tweets
+    @user = User.find(session['user_id'])
+    @profile_owner = User.find(session['user_id'])
+    erb :'profile/profile_page'
   else
     redirect '/login'
   end
+end
+
+get '/users/:id' do
+  if user_logged_in?
+    @user = User.find(session['user_id'])
+  else
+    @user = User.new(email: 'Not Logged In')
+  end
+  @profile_owner = User.find(params[:id])
+  erb :'profile/profile_page'
 end
 
 ### I don't think this actually works right now. ctg ###
