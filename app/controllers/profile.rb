2fccfdeab026896bffab43af15dcbade2599549e
Charles <giardina.charles@gmail.com>
2015-01-16 08:19:55 -0800

    bug fix. users newly created tweets not showing up on their profiile page

--- app/controllers/profile.rb
+++ app/controllers/profile.rb
@@ -1,17 +1,5 @@
 get '/profile' do
-  if user_logged_in?
-    p 'profile owner'
-    p @profile_owner = User.find(session['user_id'])
-    p 'session id'
-    p session['user_id']
-    p 'that persons tweets'
-    p @profile_owner.tweets
-    @user = User.find(session['user_id'])
-    @profile_owner = User.find(session['user_id'])
-    erb :'profile/profile_page'
-  else
-    redirect '/login'
-  end
+    redirect "/users/#{session['user_id']}"
 end
 
 get '/users/:id' do
--- app/controllers/tweet.rb
+++ app/controllers/tweet.rb
@@ -2,8 +2,8 @@
 
 post "/tweets/new" do
   if user_logged_in?
-	  @tweets = Tweet.create(params[:tweet])
-    redirect '/'
+	  @tweets = Tweet.create(content: params[:tweet][:content], user_id: session['user_id'])
+    redirect '/profile'
   else
     redirect '/login'
   end
@@ -31,7 +31,7 @@ end
 
 post "/retweet" do
   if user_logged_in?
-    tweet = Tweet.new(content: params[:content], user_id: session['user_id'], parent_id: params[:parent_id])
+    tweet = Tweet.new(content: params[:tweet][:content], user_id: session['user_id'], parent_id: params[:parent_id])
     tweet.save
     redirect '/'
   else
